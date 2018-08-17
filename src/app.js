a5f8ea54b238744721f0fc9363346fc942d59248
kevinjc <kevinjc@uio.no>
2018-08-17 14:31:25 +0200

    initial setup of router to different component pages

--- src/app.js
+++ src/app.js
@@ -1,8 +1,47 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my Expense component
+    </div>
+);
+
+const EditExpensePage = () => {
+    <div>
+        This is from my edit Expense component
+    </div>
+};
+
+const HelpPage = () => {
+    <div>
+        HELP
+    </div>
+};
+
+
+
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />   
+            <Route path="/edit" component={EditExpensePage} />    
+            <Route path="/help" component={HelpPage} />    
+        </div>    
+    </BrowserRouter>
+)
+
 const appRoot = document.getElementById('app');
-ReactDOM.render(<p>boilerplate</p>, appRoot);
+ReactDOM.render(routes, appRoot);
