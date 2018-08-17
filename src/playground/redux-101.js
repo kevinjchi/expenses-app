50d906429774f14bf218dabb7d882562c9a735ab
kevinjc <kevinjc@uio.no>
2018-08-18 01:05:10 +0200

    dispatch actions to store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,10 +1,37 @@
 import { createStore} from 'redux';
 
 // set state to default state object
-const store = createStore((state = {count: 0}) => {
-    return state;
+const store = createStore((state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default: 
+            return state;
+    }
+});
+
+// I'd like to increment the count
+store.dispatch({
+    type: 'INCREMENT'
+});
+// dispatch count to 2
+store.dispatch({
+    type: 'DECREMENT'
+});
+// reset count to 0
+store.dispatch({
+    type: 'RESET'
 });
 
 console.log(store.getState())
-
 console.log('101'); 
