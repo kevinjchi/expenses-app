4024f0b866ebd9641450c0fb07f4ba0ec84acb29
kevinjc <kevinjc@uio.no>
2018-08-18 01:15:47 +0200

    subscribing and dynamic actions in redux-store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,33 +4,53 @@ import { createStore} from 'redux';
 const store = createStore((state = {count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy == 'number' ? acrion.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy;
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            return {
+                count: state.count 
             };
         default: 
             return state;
     }
 });
 
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
 // I'd like to increment the count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
+
+unsubscribe();
 // dispatch count to 2
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decrementBy: 10
 });
 // reset count to 0
 store.dispatch({
     type: 'RESET'
+});
+
+
+store.dispatch({
+    type: 'SET',
+    count: 101
 });
 
 console.log(store.getState())
