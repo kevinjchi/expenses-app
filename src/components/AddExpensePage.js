6fecc49c6d1ae42eaa19525b81ad8876dcc96766
kevinjc <kevinjc@uio.no>
2018-08-19 13:33:48 +0200

    wiring up expense app to form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
-const AddExpensePage = () => (
+import { connect } from 'react-redux';
+import { addExpense } from '../actions/expenses';
+
+const AddExpensePage = (props) => (
     <div>
         This is from my Expense component
-        <ExpenseForm />
+        <ExpenseForm 
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push('/');
+            }}
+        />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
