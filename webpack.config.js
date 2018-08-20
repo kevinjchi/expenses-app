54b23c8f14bd521449bdfca4366a620a4a8e4bbc
kevinjc <kevinjc@uio.no>
2018-08-20 21:56:13 +0200

    fixing webpack, installing extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 // entry -> output 
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // console.log(path.join(__dirname, 'public'));
 
 // configuration details for webpack file
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
     return {
         entry: './src/app.js',
         output: {
@@ -21,13 +22,17 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
             }]
-        },
+        }, 
+        plugins: [
+            CSSExtract
+        ],
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
