950845b21b3f76197994cd0fed3a8eafa4dff2f1
kevinjc <kevinjc@uio.no>
2018-08-21 00:01:48 +0200

    changes to ExpenseListItem.js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
-
+import moment from 'moment';
+import numeral from 'numeral';
 // stateless functional component rendering individual expense item
 
 // can destructure ({description, amount, createdAt}) or use  props.expenses....
+numeral.locale('no');
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
     <div>
     <Link to={`/edit/${id}`}>
@@ -13,7 +15,11 @@ const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
         {description}
         </h3>
     </Link>
-        <p>- {createdAt} -{amount}</p>
+        <p>
+        {numeral(amount / 100).format('$0,0.00')} 
+        - 
+        {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
         <button onClick={(e) => {
             dispatch(removeExpense({ id }));
         }}>Remove item </button>
