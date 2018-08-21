c47f1eb4637280ded34321cfccac135275f1544d
kevinjc <kevinjc@uio.no>
2018-08-21 16:17:13 +0200

    updating startEx

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -7,6 +7,12 @@ import database from '../firebase/firebase';
 // action generator returns function
 // coponent dispatched function () 
 // function runs (has the ability to dispatch other actions and do what it wants)
+
+//  ADD_EXPENSE
+export const addExpense = (expense) => ({
+    type: 'ADD_EXPENSE',
+    expense
+});
 // promise chaining
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
@@ -29,17 +35,19 @@ export const startAddExpense = (expenseData = {}) => {
 };
 
 
-
-//  ADD_EXPENSE
-export const addExpense = (expense) => ({
-    type: 'ADD_EXPENSE',
-    expense
-});
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id    
 });
+
+export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+        database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({id}));
+        });
+    };
+};
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
@@ -48,26 +56,35 @@ export const editExpense = (id, updates) => ({
     updates
 });
 
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
+
+
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
     expenses
   });
   
-  
-  export const startSetExpenses = () => {
+
+export const startSetExpenses = () => {
     return (dispatch) => {
-      return database.ref('expenses').once('value').then((snapshot) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
         const expenses = [];
-  
+
         snapshot.forEach((childSnapshot) => {
-          expenses.push({
+            expenses.push({
             id: childSnapshot.key,
             ...childSnapshot.val()
-          });
+            });
         });
-  
+
         dispatch(setExpenses(expenses));
-      });
+        });
     };
-  };
+};
