934f8b41001e61579a2451e1c3043cccbd5c79b0
kevinjc <kevinjc@uio.no>
2018-08-23 12:37:24 +0200

    Styling list of items

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -54,9 +54,8 @@ class ExpenseForm extends Component {
     };
     render() {
         return (
-            <div>
-            {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
+            <form className="form" onSubmit={this.onSubmit}>
+            {this.state.error && <p className="form__error">{this.state.error}</p>}
                 <input
                     className="text-input"
                     type="text"
@@ -86,9 +85,10 @@ class ExpenseForm extends Component {
                     onChange={this.onNoteChange}
                 >
                 </textarea>
-                <button> Add expense </button>
+                    <div>
+                        <button className="button"> Add expense </button>
+                    </div>
                 </form>
-            </div>
         )
     };
 };
